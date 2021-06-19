4d13c4deb56a70e1ad133db421d19826b1902211
jedymatt <jedymatt@gmail.com>
2021-06-20 00:01:19 +0800

    Update tasks.py

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -3,12 +3,12 @@ import asyncio
 from discord.ext import tasks, commands
 
 from db.connector import session
+from models import Player, User
 
 
 class BackgroundTask(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        await asyncio.sleep(600)
         self.commit.start()
 
     @tasks.loop(minutes=10)
@@ -22,6 +22,7 @@ class BackgroundTask(commands.Cog):
     @commit.before_loop
     async def before_commit(self):
         await self.bot.wait_until_ready()
+        await asyncio.sleep(600)
 
     def cog_unload(self):
         self.commit.cancel()
@@ -29,6 +30,12 @@ class BackgroundTask(commands.Cog):
     @commands.command(name='commit')
     async def commit_(self, ctx):
         session.commit()
+
+    @commands.command()
+    async def delete(self, ctx):
+        player = session.query(Player).join(User, User.player_id == Player.id).where(
+            User.discord_id == ctx.author.id).one()
+        session.delete(player)
 
 
 def setup(bot: commands.Bot):
