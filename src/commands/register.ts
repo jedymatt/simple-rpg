cb4857a8f408c47e0c1ecad4cba862fe1c6ea76b
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-09-03 19:25:11 +0800

    Update register.ts

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -1,6 +1,6 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
-import {Character} from '../models/character';
+import { Character } from '../models/character';
 
 @ApplyOptions<Command.Options>({
 	description: 'Register a character',
@@ -8,7 +8,7 @@ import {Character} from '../models/character';
 export class UserCommand extends Command {
 	public override registerApplicationCommands(registry: Command.Registry) {
 		registry.registerChatInputCommand((builder) =>
-			builder //
+			builder
 				.setName(this.name)
 				.setDescription(this.description)
 		);
@@ -16,15 +16,21 @@ export class UserCommand extends Command {
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
 		if (await Character.exists({ discordId: interaction.user.id })) {
-			await interaction.reply({ content: 'You already have a character registered!', ephemeral: true });
+			return await interaction.reply({ content: 'You already have a character registered!', ephemeral: true });
 		}
 
 		await Character.create({
 			discordId: interaction.user.id,
 			level: 1,
-			exp: 0
+			exp: 0,
+			attributes: {
+				hp: 50,
+				strength: 15,
+				defense: 5,
+			},
+			location: 'hometown',
 		});
-		
+
 
 		return interaction.reply({ content: 'Character successfully registered.' });
 	}
