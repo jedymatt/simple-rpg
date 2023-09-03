a54a6b5040527f10ce4fa5706506edeb0bf85850
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-09-03 19:25:51 +0800

    Create monster.ts

---
+++ src/models/monster.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+interface IMonster {
+    name: string;
+    slug: string;
+    description: string;
+    attributes: {
+        hp: number;
+        strength: number;
+        defense: number;
+    };
+}
+
+const monsterSchema = new mongoose.Schema<IMonster>({
+    name: { type: String, required: true },
+    slug: { type: String, required: true },
+    description: { type: String, required: true },
+    attributes: {
+        hp: { type: Number, required: true },
+        strength: { type: Number, required: true },
+        defense: { type: Number, required: true },
+    },
+});
+
+const Monster = mongoose.model<IMonster>("Monster", monsterSchema);
+
+export { Monster };
