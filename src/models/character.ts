831a080312ba38033df50b6c4b3cd18986570182
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-09-03 19:25:31 +0800

    Update character.ts

--- src/models/character.ts
+++ src/models/character.ts
@@ -1,12 +1,29 @@
 import mongoose from 'mongoose';
 
-const Character = mongoose.model(
-    'Character',
-    new mongoose.Schema({
-        discordId: String,
-        level: Number,
-        exp: Number
-    })
-);
+interface ICharacter {
+	discordId: string;
+	level: number;
+	exp: number;
+	attributes: {
+		hp: number;
+		strength: number;
+		defense: number;
+	};
+	location: String;
+}
+
+const characterSchema = new mongoose.Schema<ICharacter>({
+	discordId: { type: String, required: true },
+	level: { type: Number, required: true },
+	exp: { type: Number, required: true},
+	attributes: {
+		hp: {type: Number, required: true},
+		strength: {type: Number, required: true},
+		defense: {type: Number, required: true},
+	},
+	location: { type: String, required: true }
+});
+
+const Character = mongoose.model<ICharacter>('Character', characterSchema);
 
 export { Character };
