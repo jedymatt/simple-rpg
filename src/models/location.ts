c96ca266892abfc1cc6af3b8ffb7f9f6a7b94ef9
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-09-04 14:45:31 +0800

    Update location.ts

--- src/models/location.ts
+++ src/models/location.ts
@@ -1,19 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import type { IMonster } from './monster';
 
 interface ILocation {
-    name: string;
-    slug: string;
-    description: string;
-    monsters: string[];
+	name: string;
+	description: string;
+	levelRequirement: number;
+	monsters: mongoose.PopulatedDoc<mongoose.Document<mongoose.ObjectId[]> & IMonster[]>;
 }
 
 const locationSchema = new mongoose.Schema<ILocation>({
-    name: { type: String, required: true },
-    slug: { type: String, required: true },
-    description: { type: String, required: true },
-    monsters: { type: [String], required: true }
+	name: { type: String, required: true, unique: true },
+	description: { type: String, required: false },
+	levelRequirement: { type: Number, required: true },
+	monsters: { type: [mongoose.Schema.Types.ObjectId], ref: 'Monster', required: true }
 });
 
-const Location = mongoose.model<ILocation>("Location", locationSchema);
+const Location = mongoose.model<ILocation>('Location', locationSchema);
 
 export { Location };
+export type { ILocation };
